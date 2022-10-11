b48acd3ed1a197477606534371881a182f0218c6
Chimda Brignoli <chimdabrignoli@gmail.com>
2022-10-11 21:12:55 +0200

    clear console

--- scripts/pages/search.js
+++ scripts/pages/search.js
@@ -5,16 +5,15 @@ let applianceResult=[];
 let ustensilsResult=[];
 let valueSearched;
 let recipesCardFiltered = [];
+
 // DOM elements
 const searchBar = document.querySelector("#search-input");
 const cardCont = document.querySelector("#list-recipes");
-console.log("cardCont", cardCont);
-//let recipeCard = document.getElementsByClassName("card-recipe");
 let recipeCard = cardCont.children;
-//console.log("recipeCard arr", recipeCard);
-console.log("recipeCard array", recipeCard);
+
 // EVENTS
 searchBar.addEventListener("keyup", recipesFiltered);
+
 /**
  * Display card of recipes filtered after typing into the search bar
  * @param {Event} event 
@@ -22,7 +21,6 @@ searchBar.addEventListener("keyup", recipesFiltered);
 function recipesFiltered(event) {
     
     valueSearched = event.target.value.toLowerCase();
-    console.log("value input", valueSearched);
     let numberOfChar = valueSearched.length;
     card = recipeCard;
     
@@ -46,18 +44,11 @@ function mainResearch(valueSearched, recipeCard) {
     allListAppliances=[];
     allListUstensils=[];
     recipesCardFiltered=[]
-    //console.log("recipeCard", recipeCard)
    
         for (let i=0; i < recipeCard.length; i++) {
             if ( recipeCard[i].textContent.toLowerCase().includes(valueSearched)) {
                 recipeCard[i].style.display = "block"; 
-                let idCard = recipeCard[i].dataset["id"];
-                //console.log("idCard", idCard)
                 recipesResult.push(recipeCard[i]);
-                //console.log("recipesResult", recipesResult);
-                
-                
-
 
                 // liste ingrédients
                 let ingr= recipeCard[i].getElementsByClassName("ingr");
@@ -65,14 +56,11 @@ function mainResearch(valueSearched, recipeCard) {
                     let ingrText = ingr[i].textContent.toLowerCase();
                     let ingredient = ingrText.split(":")[0];
                     allListIngredients.push(ingredient);
-                    //console.log("ingredientResult", allListIngredients)
                 }
 
                 //liste appareils
                 let appl = recipeCard[i].dataset.appliance.toLowerCase();
-                //console.log("appl id", appl);
                 allListAppliances.push(appl);
-                //console.log("all list appl", allListAppliances)
 
                 // liste ustensils
                 let ust = recipeCard[i].dataset.ustensils.toLowerCase();
@@ -80,27 +68,21 @@ function mainResearch(valueSearched, recipeCard) {
                 for (let i =0; i < ustModified.length; i ++) {
                     let ustensil = ustModified[i].toLowerCase();
                     allListUstensils.push(ustensil);
-                    //console.log(allListUstensils)
                 }
             } 
             else {
                 recipeCard[i].style.display = "none";
-            } 
-
-               
+            }     
         }
+
         for ( let recipe of recipesResult) {
             let idCard = recipe.dataset["id"];
-            console.log("idqard", idCard);
             recipesArray.find(elt => {
                 if (elt.id == idCard){
                     recipesCardFiltered.push(elt)  
             }})
-            console.log("recipes card arr", recipesCardFiltered)
-       
-        }        
-        console.log("recipes result ext ", recipesResult);
-      
+        }
+                
         if (recipesResult.length == 0) {
             cardCont.innerHTML = `Aucune recette ne correspond à votre critère... vous pouvez chercher "tarte aux pommes", "poisson", etc...`
         }
